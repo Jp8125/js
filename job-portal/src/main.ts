a3d68fed5ebebde014c97c679c2575100d8e98e9
jay <jayprajapati059@gmail.com>
2023-03-24 08:22:14 +0530

    file changed

--- job-portal/src/main.ts
+++ job-portal/src/main.ts
@@ -1,30 +1,38 @@
 import * as apllicant from "./applicant"
 import { createvac } from "./hr";
-import { vacarr } from "./vac";
-let vac:string=""
-
-for (const item of vacarr) {
-      vac+=`<div class="card">
-      <div class="card-body">
-        <h4 class="card-title">Department:${item.vdept}</h4>
-        <p class="card-text">Role:${item.vtype}</p>
-        <p class="cadr-text">Date:${item.vdate.getDate()}/${item.vdate.getMonth()}/${item.vdate.getFullYear()}</p>
-        <div class="container d-flex justify-content-center"><button id="btn">Apply</button></div>
-      </div>
-    </div>`
+import { Vacancy, vacarr } from "./vac";
+function display(vacarr:Vacancy[]):void {
+  let vac:string=""
+  let i:number=0;
+  for (const item of vacarr) {
+        vac+=`<div class="card">
+        <div class="card-body">
+          <h4 class="card-title">Department:${item.vdept}</h4>
+          <p class="card-text">Role:${item.vtype}</p>
+          <p class="cadr-text">Date:${item.vdate.getDate()}/${item.vdate.getMonth()}/${item.vdate.getFullYear()}</p>
+          <div class="container d-flex justify-content-center"><button id="btn${i}">Apply</button></div>
+        </div>
+      </div>`
+      i++;
+      console.log(vac)
+      document.getElementById(
+        "app"
+      )!.innerHTML = vac;
+  }
+  
 }
-console.log(vac)
-document.getElementById(
-  "app"
-)!.innerHTML = vac;
-var btn=document.getElementById("btn");
-btn?.addEventListener("click",()=>{
-   apllicant.apply()
-   console.log(apllicant.adetails)
-})
+display(vacarr);
+for (let i = 0; i < vacarr.length; i++) {
+  var btn=document.getElementById("btn"+i);
+  btn?.addEventListener("click",()=>{
+     apllicant.apply()
+     console.log(apllicant.adetails)
+  })
+}
 var cv=document.getElementById("create");
 cv?.addEventListener("click",()=>{
   console.log(vacarr)
   createvac();
+  display(vacarr)
 })
 
